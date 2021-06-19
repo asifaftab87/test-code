6addc78577caf7b927267103ee840caa2700eb49
asifaftab87 <asifaftab87@gmail.com>
2021-06-19 19:31:31 +0800

    adding some of examples

--- root/test-code/src/main/java/org/la/test/code/hackr/rank/ExtraLongFactorial.java
+++ root/test-code/src/main/java/org/la/test/code/hackr/rank/ExtraLongFactorial.java
@@ -5,13 +5,13 @@ import java.math.BigInteger;
 public class ExtraLongFactorial {
 
     public static void main(String[] args) {
-        extraLongFactorials(30);
+        extraLongFactorials(50);
     }
 
     public static void extraLongFactorials(int n) {
 
         BigInteger result = null;
-        BigInteger factorial = new BigInteger("1");
+        BigInteger factorial = BigInteger.valueOf(1);
         int t = n;
         while(t>1){
             factorial = factorial.multiply(BigInteger.valueOf(t--));
---
+++ root/test-code/src/main/java/org/la/test/code/karumanchi/back/tracking/AllStringBits.java
@@ -0,0 +1,4 @@
+package org.la.test.code.karumanchi.back.tracking;
+
+public class AllStringBits {
+}

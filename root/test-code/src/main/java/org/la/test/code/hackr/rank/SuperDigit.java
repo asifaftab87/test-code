eddfb1f5a3d9a14029ac6a3701b80c211cbd4b17
asifaftab87 <asifaftab87@gmail.com>
2021-08-01 11:11:40 +0800

    url of github page

--- root/test-code/src/main/java/org/la/test/code/hackr/rank/SuperDigit.java
+++ root/test-code/src/main/java/org/la/test/code/hackr/rank/SuperDigit.java
@@ -7,15 +7,16 @@ import java.util.stream.IntStream;
     here is a formula that
     sum of digit of any number again and again till reach to single digit num
     is: divide number by 9
-        if remainder is zero then the last digit of recursive sum is 9
-        if remainder is non zero then that is the single digit sum
-        formula is
-        n = number, k = number of times to repeat ur number
-        r = (n*k) % 9
-        result = (r==0) ? 9 : r;
-        formula
-        http://applet-magic.com/digitsummod9.htm
-        https://www.khanacademy.org/math/in-in-class-6th-math-cbse/x06b5af6950647cd2:playing-with-numbers/x06b5af6950647cd2:tests-for-divisibility/v/the-why-of-the-9-divisibility-rule
+    if remainder is zero then the last digit of recursive sum is 9
+    if remainder is non zero then that is the single digit sum
+    formula is
+    n = number, k = number of times to repeat ur number
+    r = (n*k) % 9
+    result = (r==0) ? 9 : r;
+    formula
+    http://applet-magic.com/digitsummod9.htm
+    https://www.khanacademy.org/math/in-in-class-6th-math-cbse/x06b5af6950647cd2:playing-with-numbers/x06b5af6950647cd2:tests-for-divisibility/v/the-why-of-the-9-divisibility-rule
+    url of super digit hackerrank code: https://github.com/asifaftab87/test-code/blob/main/root/test-code/src/main/java/org/la/test/code/hackr/rank/SuperDigit.java
  */
 public class SuperDigit {
     public static int superDigit(String n, int k) {
